1a2ab557bc46ef85a66b4af70d0042d25e0e7296
lorange <jglorange@gmail.com>
2020-05-19 18:20:57 +0000

    update

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ I felt the need to create a child component => ResultItem
 I hope that's okay.
 
 For styling I used Styled Components and CSS variables.
+
+When an item is selected, it logs to the console.
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./App.css";
-import TaktikalSearch from "./components/Taktikal";
+import TaktikalSearch from "./components/TaktikalSearch";
 
 function App() {
   return (
